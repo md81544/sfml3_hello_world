c9d92ff7f19ecbbc98bf93f2cf17a678d8088063
Martyn Davis <mcd@marengo.cc>
2025-07-19 07:36:18 +0100

    Handle key presses

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
 
     const sf::Font font("font.ttf");
     sf::Text text(font, "Hello world", 50);
-    text.setPosition({50, 50});
+    text.setPosition({ 50, 50 });
 
-    while (window.isOpen())
-    {
-        // Process events
-        while (const std::optional event = window.pollEvent())
-        {
-            // Close window: exit
-            if (event->is<sf::Event::Closed>())
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
+            const auto keyEvent = event->getIf<sf::Event::KeyPressed>();
+            if (keyEvent) {
+                // Ctrl-Q will close also
+                if (keyEvent->code == sf::Keyboard::Key::Q && keyEvent->control) {
+                    window.close();
+                }
+            }
         }
 
         // Clear screen
